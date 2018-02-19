b2ce5ac58b4909cb57a5f8f8283db5d2b0afb86b
Shivvyyy <smarty.shibyan@gmail.com>
2018-02-19 16:37:40 +0530

    shivy pushed multi images

---
+++ api/models/product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: {type: String, required: true},
+  price: {type: Number, required :true},
+  productImage: {type: Array, required:true},
+  mainCategory: {type: String, required: true},
+  subCategory: {type:String, required: true},
+  onSale: {type:Boolean, required: false},
+  featured: {type:Boolean, required: false},
+  bestSeller: {type:Boolean, required: false},
+  hotDeals: {type:Boolean, required: false}
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
